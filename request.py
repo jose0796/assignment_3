1dbf671d06bc001ff7139b3fbea2f6a44e669077
jose0796 <jmoran071996@gmail.com>
2020-02-29 20:47:10 -0500

    Request to network device

--- request.py
+++ request.py
@@ -4,8 +4,16 @@ import requests
 from pprint import pprint
 
 response = requests.post(
-    'https://sandboxnac.cisco.com/dna/system/api/v1/auth/token',
-    headers={'Authorization':'Basic ZGV2bmV0dXN1cjpDaXNjbzEyMyE='})
+    'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
+    auth=('devnetuser', 'Cisco123!'))
     
+payload=response.json()
+pprint(payload)
+
+token=response.json()['Token']
+response = requests.get(
+    'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+    headers={'X-Auth-Token':token}
+)
 payload=response.json()
 pprint(payload)
