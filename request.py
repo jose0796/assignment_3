04d2ce0d7e1d21d27929c0c2a5354c46f79dddc2
jose0796 <jmoran071996@gmail.com>
2020-02-29 20:51:27 -0500

    Adds exception handling for failed responses

--- request.py
+++ request.py
@@ -4,8 +4,9 @@ import requests
 from pprint import pprint
 
 response = requests.post(
-    'https://sandboxnac.cisco.com/dna/system/api/v1/auth/token',
-    auth=('devnetuser','Cisco123!'))
+    'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
+    auth=('devnetuser', 'Cisco123!'))
     
+response.raise_for_status()
 payload=response.json()
 pprint(payload)
